a2653418f968827be632c11b0f2acec40cd0e7f6
supah <andrecostafrancacf@gmail.com>
2024-09-29 16:17:14 -0300

    made the names shorter
    better optimization (screw legibility)

--- semaforos4.c
+++ semaforos4.c
@@ -5,33 +5,33 @@
 int main(){
     setlocale(LC_ALL, "Portuguese");
 
-    int semaforo1 = 5;
-    int semaforo2 = 20;
-    int semaforo3 = 20;
-    int semaforo4 = 30;
+    int s1 = 5;
+    int s2 = 20;
+    int s3 = 20;
+    int s4 = 30;
 
-    printf("Semáforo 1:\n");
-    while (semaforo1 > 0) {
-        printf("\r%02d", semaforo1);
+    printf("SemÃ¡foro 1:\n");
+    while (s1 > 0) {
+        printf("\r%02d", s1);
 
         clock_t stop = clock() + CLOCKS_PER_SEC;
         while (clock() < stop){}
 
-        semaforo1--;
+        s1--;
     }
 
     printf("\rFechado! Pedestres podem atravessar.");
 
     printf("\n\n");
 
-    printf("Semáforo 2:\n");
-    while (semaforo2 > 0) {
-        printf("\r%02d", semaforo2);
+    printf("SemÃ¡foro 2:\n");
+    while (s2 > 0) {
+        printf("\r%02d", s2);
 
         clock_t stop = clock() + CLOCKS_PER_SEC;
         while (clock() < stop){}
 
-        semaforo2--;
+        s2--;
     }
 
     printf("\rAberto!");
